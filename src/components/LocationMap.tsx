af55655e0d9cb38bb9b013b873292cc8d491f692
Juan Marcos <xmarcos@fi.muni.cz>
2024-08-26 18:03:11 +0200

    icon

--- src/components/LocationMap.tsx
+++ src/components/LocationMap.tsx
@@ -1,5 +1,7 @@
 import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Pin from "../photos/pin.png"
+import L from 'leaflet';
 
 const LocationMap = () => {
     const position: [number, number] = [49.405694, 19.476371]; // Example coordinates (Eiffel Tower)
@@ -9,7 +11,12 @@ const LocationMap = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-        <Marker position={position}>
+        <Marker position={position} icon={new L.Icon({
+            iconUrl: Pin,
+            iconSize: [32, 32],
+            iconAnchor: [16, 32],
+            popupAnchor: [0, -28]
+        })}>
                 <Popup>
                     Ortopedická ambulancia <br /> Michal Prádel - Námestovo. <br /> <br /> Červeného Kríža 62/30, 02901 Námestovo
                 </Popup>
