c8f1c173df4e9239dcf7840c10b52d9a0995419d
Juan Marcos <xmarcos@fi.muni.cz>
2024-09-08 20:21:54 +0200

    declaration

---
+++ src/custom.d.ts
@@ -0,0 +1,10 @@
+declare module "*.webp" {
+  const value: string;
+  export default value;
+}
+
+// You can also add other image types if needed
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
